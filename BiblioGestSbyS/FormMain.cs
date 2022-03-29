011f56eed3fd62368bbe1e86751f8d29854343e8
laurentbedu <bedulaurent@gmail.com>
2022-03-29 08:53:22 +0200

    ok

--- BiblioGestSbyS/FormMain.cs
+++ BiblioGestSbyS/FormMain.cs
@@ -1,5 +1,3 @@
-using BiblioGestSbyS.Models;
-
 namespace BiblioGestSbyS
 {
     public partial class FormMain : Form
@@ -11,11 +9,11 @@ namespace BiblioGestSbyS
 
         private void FormMain_Load(object sender, EventArgs e)
         {
-            List<Exemplaire> exemplaires = Exemplaire.jsonDataAcces.GetAll();
-            List<Livre> livres = Livre.jsonDataAcces.GetAll();
+            //List<Exemplaire> exemplaires = Exemplaire.jsonDataAcces.GetAll();
+            //List<Livre> livres = Livre.jsonDataAcces.GetAll();
             //Exemplaire exemplaire1 = Exemplaire.jsonDataAcces.GetById(1);
-            Livre livre = new Livre() { Isbn = "abcd", Titre = "Coucou" };
-            Livre.jsonDataAcces.Persist(livre);
+            //Livre livre = new Livre() { Isbn = "abcd", Titre = "Coucou" };
+            //Livre.jsonDataAcces.Persist(livre);
             //Livre livre1 = new Livre() { Isbn = "1235-4589-8925", Titre = "Hello !" };
         }
     }
